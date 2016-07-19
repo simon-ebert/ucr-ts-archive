1ede28502428d0715963d406881c68bccd2786fd
Simon Ebert <ebert1simon@gmail.com>
2016-07-19 22:40:34 +0200

    Initial commit

--- Test.py
+++ Test.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import pandas as pd
+from euclidean import oneNN
+from helpers import loadDataset
+
+
+# load dataset
+dataset = "BeetleFly"
+testData, trainData = loadDataset(dataset)
+
+# Array of classes
+classes = trainData[0].unique()
+
+# Dict of averaged class representatives
+d = dict()
+for c in classes:
+    d[c-1] = trainData[trainData[0]==c].mean()
+averagedTrainData = pd.DataFrame.from_dict(d, orient="index")
+
+print(testData.head())
+print(averagedTrainData.head())
+
+print(oneNN(testData,averagedTrainData))
---
+++ euclidean.py
@@ -0,0 +1,24 @@
+import numpy as np
+import sys
+
+
+def oneNN(testData, trainData):
+    length = len(testData.columns)
+    results = dict()
+
+    for i in range(len(testData)):
+        test = testData.ix[i]
+        minDist = sys.maxsize
+        for j in range(len(trainData)):
+            train = trainData.ix[j]
+            dist = np.linalg.norm(train[1:] - test[1:])
+            if dist < minDist:
+                minDist = dist
+                results[i] = train[0]
+
+    # calculate error rate
+    errors = 0
+    for i in range(len(testData)):
+        if results[i] != testData[0][i]:
+            errors += 1
+    return errors / len(testData)
